19f4412665ac9d8f323aa22d88f3d12aae45cc90
Musa Halilov <halilovmusa@gmail.com>
2019-05-06 23:33:55 +0300

    Удален не нужный метод в DB_Broker CreateDialog

--- src/main/java/com/mshl/DB_Broker/DB_Broker.java
+++ src/main/java/com/mshl/DB_Broker/DB_Broker.java
@@ -2,14 +2,5 @@ package com.mshl.DB_Broker;
 
 public class DB_Broker
 {
-
-    /**
-     * Создает диалог пользователей.
-     * @return Возвращается id нового диалога. Если возникнет ошибка в БД, то
-     * возвращается -1.
-     */
-    public int CreateDialog(int user1_id, int user2_id)
-    {
-        return 0;
-    }
+    
 }
