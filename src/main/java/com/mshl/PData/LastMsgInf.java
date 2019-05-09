c22b1649f3db02040f30a8309df7245a3e859b1c
Musa Halilov <halilovmusa@gmail.com>
2019-05-09 06:51:34 +0300

    Прописаны внутренности классов LastMsgInf & MessageInf

--- src/main/java/com/mshl/PData/LastMsgInf.java
+++ src/main/java/com/mshl/PData/LastMsgInf.java
@@ -1,4 +1,29 @@
 package com.mshl.PData;
 
-public class LastMsgInf {
+import java.sql.Timestamp;
+
+public class LastMsgInf
+{
+    public LastMsgInf(int from_user_id, Timestamp last_read_time, Timestamp last_msg_time, Timestamp my_last_reading_time)
+    {
+        this.from_user_id = from_user_id;
+        this.last_read_time = last_read_time;
+        this.last_msg_time = last_msg_time;
+        this.my_last_reading_time = my_last_reading_time;
+    }
+
+    @Override
+    public String toString() {
+        return "LastMsgInf{" +
+                "from_user_id=" + from_user_id +
+                ", last_read_time=" + last_read_time +
+                ", last_msg_time=" + last_msg_time +
+                ", my_last_reading_time=" + my_last_reading_time +
+                '}';
+    }
+
+    private int from_user_id;
+    private Timestamp last_read_time;
+    private Timestamp last_msg_time;
+    private Timestamp my_last_reading_time;
 }
--- src/main/java/com/mshl/PData/MessageInf.java
+++ src/main/java/com/mshl/PData/MessageInf.java
@@ -1,4 +1,32 @@
 package com.mshl.PData;
 
-public class MessageInf {
+import java.sql.Timestamp;
+
+public class MessageInf
+{
+    public MessageInf(int from_user_id, String from_user_name, String from_user_avatar, String msg, Timestamp time)
+    {
+        this.from_user_id = from_user_id;
+        this.from_user_name = from_user_name;
+        this.from_user_avatar = from_user_avatar;
+        this.msg = msg;
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "MessageInf{" +
+                "from_user_id=" + from_user_id +
+                ", from_user_name='" + from_user_name + '\'' +
+                ", from_user_avatar='" + from_user_avatar + '\'' +
+                ", msg='" + msg + '\'' +
+                ", time=" + time +
+                '}';
+    }
+
+    private int from_user_id;
+    private String from_user_name;
+    private String from_user_avatar;
+    private String msg;
+    private Timestamp time;
 }
