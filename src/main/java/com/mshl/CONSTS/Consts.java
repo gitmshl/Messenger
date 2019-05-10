913c74626304cb36fef75a5bb6ae9c16583705d9
Musa Halilov <halilovmusa@gmail.com>
2019-05-10 06:45:41 +0300

    В Consts добавлено описание DATA_TYPE_3 (передача сообщений)

--- src/main/java/com/mshl/CONSTS/Consts.java
+++ src/main/java/com/mshl/CONSTS/Consts.java
@@ -7,7 +7,18 @@ public class Consts
 {
     public static final int SERVER_ID = -1;
     public static final int DATA_TYPE_TEXT = 0; /// data = just string
+    /*
+        data = "some string..."
+     */
     public static final int DATA_TYPE_MESSAGE = 1; /// data = {name: , avatar: , msg: }
+    /*
+        data =
+        {
+            name: ,
+            avatar: ,
+            msg: ,
+        }
+     */
     public static final int DATA_TYPE_DIALOGS_LIST = 2;
     /*
          data =
@@ -32,7 +43,28 @@ public class Consts
     */
     public static final int DATA_TYPE_MESSAGES_LIST = 3;
     /*
-
+        data =
+        {
+            lastMsgInf:
+            {
+                from_user_id: ,
+                last_read_time: ,
+                last_msg_time: ,
+                my_last_reading_time:
+            },
+            messageInfList: [
+                {
+                    from_user_id: ,
+                    from_user_name: ,
+                    from_user_avatar: ,
+                    msg: ,
+                    time:
+                },
+                {
+                    ...
+                }
+            ]
+        }
      */
     public static final List<Integer> Protocol_Codes = new ArrayList<>();
 
